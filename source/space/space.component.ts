d95bedffb81a4a627e737f6c745435c2af6cf236
SamGraber <authorsamgraber@gmail.com>
2016-09-29 15:46:04 -0400

    Show visible terrain with a color

--- source/space/space.component.ts
+++ source/space/space.component.ts
@@ -11,4 +11,12 @@ import { Space } from '../types/space';
 export class SpaceComponent {
 	@Input() space: Space;
 	@Output() select: EventEmitter<void> = new EventEmitter<void>();
+
+	get spaceClass(): string {
+		if (this.space.visible) {
+			return this.space.terrain.class;
+		} else {
+			return '';
+		}
+	}
 }
